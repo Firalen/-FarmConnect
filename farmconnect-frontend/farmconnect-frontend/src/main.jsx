0b7d8db0f8c74eb5fc3b445f35b4de456d2eb7a9
fira <fira81564@gmail.com>
2025-07-01 23:08:05 +0300

    Remove Tailwind CSS configuration files and update index.css import: Deleted postcss.config.js and tailwind.config.js, and modified index.css to use @import for Tailwind CSS, streamlining the styling setup.

--- farmconnect-frontend/farmconnect-frontend/src/main.jsx
+++ farmconnect-frontend/farmconnect-frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
 import i18n from './i18n/index.jsx';
 import { AuthProvider } from './context/AuthContext';
 import { I18nextProvider } from 'react-i18next';
